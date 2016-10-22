f614e0a8e3aa9268f29b1d3d0dd9bee026406ce7
cmdctrl <jordan.lyon@rutgers.edu>
2016-10-22 19:39:57 -0400

    more work on switch.cpp - ranges properly setup

--- oculus/split.cpp
+++ oculus/split.cpp
@@ -5,16 +5,29 @@
 #include "ovr.h"
 
 int main(int argc, const char** argv){
-    cv::Mat img = cv::imread(argv[1], 0);
+    cv::Mat img = cv::imread(argv[1]);
 
-    if(img.empty()){
+    if(img.empty() || !img.data){
+        printf("no image\n");
         return -1;
     }
     int height = img.rows;
     int width = img.cols;
-
-    std::cout << "height is: " <<height;
+    int crop = width/20;
     
+    printf("Image dimensions: height=%d width=%d crop=%d\n", height, width, crop);
+
+    cv::Mat subImage1(img, cv::Range::all(), cv::Range(0, width-crop)); // Mat dst(Mat src, yrange, xrange)
+    cv::Mat subImage2(img, cv::Range::all(), cv::Range(crop, width));
+
+    cv::namedWindow("Left");
+    cv::imshow("Left", subImage1);
+
+    cv::namedWindow("Right");
+    cv::imshow("Right", subImage2);
+
+    cv::waitKey(0);
+
     return 0;
 }
 
